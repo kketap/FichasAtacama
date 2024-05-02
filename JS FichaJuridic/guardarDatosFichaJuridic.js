cab7452bbc4598f1ccf814b8cc3bd5797a4bfdab
kketap <107421064+kketap@users.noreply.github.com>
2024-05-02 17:21:44 -0400

    Add files via upload

--- JS FichaJuridic/guardarDatosFichaJuridic.js
+++ JS FichaJuridic/guardarDatosFichaJuridic.js
@@ -53,6 +53,48 @@ function guardarDatosFormulario1() {
         telefono2AAM: document.getElementById('telefono2AAM').value,
 
         opcionMedios: obtenerValorRadioMedios('opcionMedios'),
+        inputMediosCorreo: document.getElementById('inputMediosCorreo').value,
+
+        opcionRelacionNatuAAM: obtenerValorRadioRelacionNatuAAM('opcionRelacionNatuAAM'),
+        inputNatuAAMotros: document.getElementById('inputNatuAAMotros').value,
+
+        opcionRelacionAAM: obtenerValorRadioRelacionAAM('opcionRelacionAAM'),
+        inputRelacionAAM: document.getElementById('inputRelacionAAM').value,
+
+        opcionInfoCliente: obtenerValorRadioInfoCliente('opcionInfoCliente'),
+        inputDestinoInfoCliente: document.getElementById('inputDestinoInfoCliente').value,
+
+        bancoCLP: document.getElementById('bancoCLP').value,
+        tipoCuentaCLP: document.getElementById('tipoCuentaCLP').value,
+        nCuentaCLP: document.getElementById('nCuentaCLP').value,
+        bancoUSS: document.getElementById('bancoUSS').value,
+        tipoCuentaUSS: document.getElementById('tipoCuentaUSS').value,
+        nCuentaUSS: document.getElementById('nCuentaUSS').value,
+
+        opcionNivelRenta: obtenerValorRadioNivelRenta('opcionNivelRenta'),
+        inputFuenteRenta: document.getElementById('inputFuenteRenta').value,
+
+        opcionPatrimonio: obtenerValorRadioPatrimonio('opcionPatrimonio'),
+
+        inputActCliente: document.getElementById('inputActCliente').value,
+
+        opcionClientePEP: obtenerValorRadioClientePEP('opcionClientePEP'),
+
+        AreaPregunta1: obtenerValorRadioAreaPregunta1('AreaPregunta1'),
+        AreaPregunta2: obtenerValorRadioAreaPregunta2('AreaPregunta2'),
+        AreaPregunta3: obtenerValorRadioAreaPregunta3('AreaPregunta3'),
+        declaroInversionista: obtenerValorRadiodeclaroInversionista('declaroInversionista'),
+
+        razonCRS: document.getElementById('razonCRS').value,
+        rutCRS: document.getElementById('rutCRS').value,
+        fechaConstCRS: document.getElementById('fechaConstCRS').value,
+        nacionalidadCRS: document.getElementById('nacionalidadCRS').value,
+
+        razonPerfil : document.getElementById('razonPerfil').value,
+        rutPerfil : document.getElementById('rutPerfil').value,
+        clienteConocidoOpcion: obtenerValorRadioClienteConocidoOpcion('clienteConocidoOpcion'),
+        referenciaPerfil : document.getElementById('referenciaPerfil').value,
+
 
     };
     localStorage.setItem('datosFormulario1', JSON.stringify(datosFormulario1)); // Usar una clave única para el formulario 1
@@ -67,7 +109,10 @@ function cargarDatosFormulario1() {
             if (datosFormulario1.hasOwnProperty(key)) {
                 var value = datosFormulario1[key];
                 if (value !== undefined) {
-                    if (key === 'accionistaBenefPregunta' || key === 'opcionMedios') {
+                    if (key === 'accionistaBenefPregunta' || key === 'opcionMedios' || key === 'opcionRelacionNatuAAM' ||
+                        key === 'opcionRelacionAAM' || key === 'opcionInfoCliente' || key === 'opcionNivelRenta' || key === 'opcionPatrimonio'
+                        || key === 'opcionClientePEP' || key === 'AreaPregunta1' || key === 'AreaPregunta2' || key === 'AreaPregunta3' ||
+                        key === 'declaroInversionista' || key === 'clienteConocidoOpcion') {
                         var radioSeleccionado = document.querySelector(`input[name="${key}"][value="${value}"]`);
                         if (radioSeleccionado) {
                             radioSeleccionado.checked = true;
@@ -90,6 +135,62 @@ function obtenerValorRadioMedios(opcionMedios) {
     var radioSeleccionado = document.querySelector(`input[name="${opcionMedios}"]:checked`);
     return radioSeleccionado ? radioSeleccionado.value : undefined;
 }
+
+function obtenerValorRadioRelacionNatuAAM(opcionRelacionNatuAAM) {
+    var radioSeleccionado = document.querySelector(`input[name="${opcionRelacionNatuAAM}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioRelacionAAM(opcionRelacionAAM) {
+    var radioSeleccionado = document.querySelector(`input[name="${opcionRelacionAAM}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioInfoCliente(opcionInfoCliente) {
+    var radioSeleccionado = document.querySelector(`input[name="${opcionInfoCliente}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioNivelRenta(opcionNivelRenta) {
+    var radioSeleccionado = document.querySelector(`input[name="${opcionNivelRenta}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioPatrimonio(opcionPatrimonio) {
+    var radioSeleccionado = document.querySelector(`input[name="${opcionPatrimonio}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioClientePEP(opcionClientePEP) {
+    var radioSeleccionado = document.querySelector(`input[name="${opcionClientePEP}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioAreaPregunta1(AreaPregunta1) {
+    var radioSeleccionado = document.querySelector(`input[name="${AreaPregunta1}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioAreaPregunta2(AreaPregunta2) {
+    var radioSeleccionado = document.querySelector(`input[name="${AreaPregunta2}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioAreaPregunta3(AreaPregunta3) {
+    var radioSeleccionado = document.querySelector(`input[name="${AreaPregunta3}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadiodeclaroInversionista(declaroInversionista) {
+    var radioSeleccionado = document.querySelector(`input[name="${declaroInversionista}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
+function obtenerValorRadioClienteConocidoOpcion(clienteConocidoOpcion) {
+    var radioSeleccionado = document.querySelector(`input[name="${clienteConocidoOpcion}"]:checked`);
+    return radioSeleccionado ? radioSeleccionado.value : undefined;
+}
+
 
 // Llamar a la función cargarDatosFormulario1 al cargar la página
 document.addEventListener('DOMContentLoaded', function () {
